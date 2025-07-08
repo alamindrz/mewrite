c239266da5f2ecb839057cb359bec86ab623aeb8
Alamin Usman Saleh <alaminusmansaleh43@gmail.com>
2025-07-08 17:33:11 +0100

    wsgi settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add your project directory to the sys.path
+path = '/home/alamindrz/mewrite'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set the settings module to your actual settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
